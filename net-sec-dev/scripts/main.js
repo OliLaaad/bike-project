0603c509a38ac5cf6e11398d4ff85870ec709056
oli <oli@realise.com>
2016-01-08 15:54:03 +0000

    Setup Complete

---
+++ net-sec-dev/scripts/main.js
@@ -0,0 +1,16 @@
+
+
+
+// media query event handler
+if (matchMedia) {
+    var mq = window.matchMedia("(min-width: 992px)");
+    mq.addListener(WidthChange);
+    WidthChange(mq);
+}
+
+// media query change
+function WidthChange(mq) {
+  
+  
+}
+
