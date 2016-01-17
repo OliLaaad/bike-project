1e3f70409682b76f925092641963381c57fc6a64
Oli Haley <olihaleypro@gmail.com>
2016-01-17 16:37:33 +0000

    Wrestling with JS

--- net-sec-dev/scripts/main.js
+++ net-sec-dev/scripts/main.js
@@ -1,98 +1,88 @@
 
 
-    var msnry = new Masonry('.grid', {
-        itemSelector: '.grid-item'
-    });
 
+var cols = document.getElementsByClassName('grid-item');
 
+  
 
-
-    window.onload = function () {
-
+document.addEventListener('DOMContentLoaded', function () {
 
         var x = 0;
         var big = 0;
 
-
-        msnry.on("layoutComplete", function () {
-
-            WidthMidChange(mq);
-            WidthLgChange(mqlg);
+        WidthMidChange();
+        var msnry = new Masonry('.grid', {
+            itemSelector: '.grid-item'
         });
 
-        // media query event handler
-        if (matchMedia) {
-            var mq = window.matchMedia("(min-width: 992px)");
-            var mqlg = window.matchMedia("(min-width: 1200px)");
-            mq.addListener(WidthMidChange);
-            mqlg.addListener(WidthLgChange);
-            WidthMidChange(mq);
-            WidthLgChange(mqlg);
-        }
+    });
 
-        // media query change
-        function WidthMidChange(mq) {
+if (matchMedia) {
 
-            if (window.innerWidth <= 991) {
-                setHeight(2, 'grid-item col-sm-6');
-            }
-            else {
-                setHeight(3, 'grid-item col-md-4');
-            }
-        }
-
-        function WidthLgChange(mqlg) {
-
-            if (window.innerWidth <= 1199) {
-                setHeight(3, 'grid-item col-md-4');
-            }
-            else {
-                setHeight(4, 'grid-item col-lg-3');
-            }
+    var mqsm = window.matchMedia("(min-width: 992px)");
+    var mq = window.matchMedia("(min-width: 992px)");
+    var mqlg = window.matchMedia("(min-width: 1200px)");
+    mq.addListener(WidthMidChange);
+    mqlg.addListener(WidthMidChange);
+    mqsm.addListener(WidthMidChange);
+}
 
 
-        }
+    function WidthMidChange() {
 
-
-        function setHeight(colNo, colClass)
-        {
-            cols = document.getElementsByClassName(colClass);
-            x = 0;
-            big = 0;
-
+        if(window.outerWidth <= 767 )
+        {            
             for (i = 0; i < cols.length; i++) {
 
-                cols[i].style.minHeight ="initial";
+                cols[i].style.height = "auto";
             }
-            for (i = 0; i < cols.length; i++) {
+        }
+        else if (window.outerWidth <= 991) {
+            setHeight(2, 'grid-item');
+        }
+        else if (window.outerWidth <= 1199) {
+            setHeight(3, 'grid-item');
+        }
+        else {
+            setHeight(4, 'grid-item');
+        }
+    }
 
-                cols[i].style.Height = "none";
+
+
+
+    function setHeight(colNo, colClass) {
+        x = 0;
+        big = 0;
+
+        for (i = 0; i < cols.length; i++) {
+
+            cols[i].style.height = "auto";
+        }
+       
+
+
+        for (i = 0; i < cols.length; i++) {
+
+            if (cols[i].clientHeight >= big) {
+                big = cols[i].clientHeight;
             }
 
+            if (i % colNo == 0 && i != 0) {
 
-            for (i = 0; i < cols.length; i++) {
-
-                if (cols[i].clientHeight > big) {
-                    big = cols[i].clientHeight;
-                }
-
-                if (i % colNo == 0 && i != 0) {
-
-                    for (t = 1; t <= colNo; t++)
-                    {
-                        c = i - t;
-                        console.log(cols[c]);
-                        cols[c].style.minHeight = big + "px";
-                        console.log('is it working');
-                    }
-
-                    console.log(big);
-                    big = 0;
+                for (t = 0; t <= colNo; t++) {
+                    c = i - t;
+                    cols[c].style.height = big + "px";
                 }
 
             }
 
         }
+     
+        
 
+    }
 
-    };
+    window.onresize = function(event) {
+        WidthMidChange();
+        };
